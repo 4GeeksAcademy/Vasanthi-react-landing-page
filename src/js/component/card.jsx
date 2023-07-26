90ee307cb2ef39964c00405d338b18564050e96b
dande100 <vasanthid10@gmail.com>
2023-07-26 09:40:35 -0500

    Styles updated

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -7,8 +7,8 @@ const Card = (props) => {
 
         <img src={props.image} className="card-img-top" alt="..." />
       </div>
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
+      <div className="card-body text-center">
+        <h5 className="card-title fs-3">{props.name}</h5>
         <p className="card-text">{props.description}</p>
       </div>
       <div class="card-footer text-muted d-flex justify-content-center">
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -7,9 +7,9 @@ const Jumbotron = () => {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Custom jumbotron</h1>
-        <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        <h1 className="display-5 fw-bold">Welcome to Movies App</h1>
+        <p className="col-md-8 fs-4">Movies App is a free app and one of the best streaming TV services you can find.  This app having one of the best collections of free movies out there, with a mix of old classics and new hit movies.</p>
+        <button className="btn btn-primary btn-lg" type="button">Start</button>
       </div>
     </div>
   )
